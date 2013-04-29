4e5f8abefd632803d641486484fff60abc034f3e
d-nnis <flashgo@gmx.net>
2013-04-30 01:05:36 +0200

    .

--- firstSteps.pl
+++ firstSteps.pl
@@ -9,19 +9,19 @@ my $png_image_file = 'd:\\workspace\\captureSnipping\\S13_mini.gif';
 my $mw = Tkx::widget->new(".");
 $mw->g_wm_title("captureSnipping");
 
-Tkx::image_create_photo('pic', -file => $png_image_file);
-my $frame = $mw->new_ttk__frame(-image => 'pic');
-#$frame->configure(-padding => "5 10");
-#$frame->configure(-borderwidth => 2, -relief => "sunken");
+my $pic_ref = Tkx::image_create_photo('pic', -file => $png_image_file);
+my $frame = $mw->new_ttk__frame();
+$frame->configure(-padding => "5 10");
+$frame->configure(-borderwidth => 2, -relief => "sunken");
 my $label = $mw->new_ttk__label(-text => "Full name:");
-$label->configure(-image => 'pic');
+$label->configure(-image => "pic");
 
 my $btn = $mw->new_ttk__button(
     -text => "Button mit PNG-Datei als Bild",
     -compound => 'top',
     -image => 'pic',
 );
-$btn->g_pack(-padx => 10, -pady => 10,);
+#$btn->g_pack(-padx => 10, -pady => 10,);
 
 Tkx::MainLoop();
 exit(0);
