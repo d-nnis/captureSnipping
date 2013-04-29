5287ad5dcc135605f7a0e4db9544f04e3e3be4ae
d-nnis <flashgo@gmx.net>
2013-04-29 21:17:32 +0200

    fails with label...

--- firstSteps.pl
+++ firstSteps.pl
@@ -7,14 +7,21 @@ Tkx::package_require('img::png');
 my $png_image_file = 'd:\\workspace\\captureSnipping\\S13_mini.gif';
 
 my $mw = Tkx::widget->new(".");
+$mw->g_wm_title("captureSnipping");
 
 Tkx::image_create_photo('pic', -file => $png_image_file);
+my $frame = $mw->new_ttk__frame(-image => 'pic');
+#$frame->configure(-padding => "5 10");
+#$frame->configure(-borderwidth => 2, -relief => "sunken");
+my $label = $mw->new_ttk__label(-text => "Full name:");
+$label->configure(-image => 'pic');
+
 my $btn = $mw->new_ttk__button(
     -text => "Button mit PNG-Datei als Bild",
     -compound => 'top',
     -image => 'pic',
 );
-$btn->g_pack(-padx => 10, -pady => 100,);
+$btn->g_pack(-padx => 10, -pady => 10,);
 
 Tkx::MainLoop();
 exit(0);
